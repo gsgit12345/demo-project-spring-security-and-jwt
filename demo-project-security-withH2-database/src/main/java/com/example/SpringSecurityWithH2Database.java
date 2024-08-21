8b61a38edf0e637ff899eef71b9ddbe4cbe3a7f3
gsgit12345 <gsgmca682@gmail.com>
2024-08-21 21:48:22 +0530

    security tutorial commit-3

--- demo-project-security-withH2-database/src/main/java/com/example/SpringSecurityWithH2Database.java
+++ demo-project-security-withH2-database/src/main/java/com/example/SpringSecurityWithH2Database.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class SpringSecurityWithH2Database
 {
     public static void main( String[] args )
     {
+        SpringApplication.run(SpringSecurityWithH2Database.class);
         System.out.println( "Hello World!" );
     }
 }
